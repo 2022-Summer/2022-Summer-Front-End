6b4ea1db975310cc37339a6aa54e0343c7847aab
开骁夏 <2337167502@qq.com>
2022-08-04 20:47:30 +0800

    test2

--- src/views/test2.vue
+++ src/views/test2.vue
@@ -1,6 +1,6 @@
 <template>
  <div style="width: 70%;margin-left: 30px;margin-top: 30px;">
- <el-button class="filter-item" type="success" icon="el-icon-download" @click="downFile()">下载</el-button>
+ <el-button class="filter-item" type="success" icon="el-icon-download" @click="downFile(id)">下载</el-button>
  </div>
 </template>
 
@@ -10,52 +10,31 @@
  export default {
  data() {
   return {
+    id:2,
   }
  },
  mounted: function() {
 
  },
  methods: {
-  downloadFile(url, options = {}){
-  return new Promise((resolve, reject) => {
-   axios({
-   method: 'post',
-   url: url, // 请求地址
-   data: options, // 参数
-   responseType: 'blob' // 表明返回服务器返回的数据类型
-   }).then(
-   response => {
-    resolve(response.data)
-    let blob = new Blob([response.data], {
-    type: 'application/vnd.ms-excel'
-    })
-    let fileNameEncode = response.headers['content-disposition'].split("filename=")[1]
-    let fileName = decodeURIComponent(fileNameEncode)
-    if (window.navigator.msSaveOrOpenBlob) {
-    navigator.msSaveBlob(blob, fileName)
-    } else {
-    var link = document.createElement('a')
-    link.href = window.URL.createObjectURL(blob)
-    link.download = fileName
-    link.click()
-    window.URL.revokeObjectURL(link.href)
+  downloadFile(){
+    this.$axios({
+        method: 'get',           /* 指明请求方式，可以是 get 或 post */
+        url: '/api/project/upload/',     /* 指明后端 api 路径，由于在 main.js 已指定根路径，因此在此处只需写相对路由 */
+        params: {
+          fileid: this.id
+        }
+        })
+        .then((res) => {
+          switch (res.data.errno){
+            case 0:
+              break;
+          }
+        })
+        .catch(err => {
+        console.log(err);         /* 若出现异常则在终端输出相关信息 */
+      });
     }
-   },
-   err => {
-    reject(err)
-   }
-   )
-  })
-  },
-  // 下载文件
-  downFile(val){
-  let postUrl= "http://localhost:8000/api/project/upload/"
-  let params = {
-   fileid: "1",
-  }
-  // console.log("下载参数",params)
-  this.downloadFile(postUrl,params)
-  },
  }
  }
 </script>
