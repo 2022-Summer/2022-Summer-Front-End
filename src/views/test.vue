a3d044c98be32b412d45d983b690d3ea0b6ddc24
开骁夏 <2337167502@qq.com>
2022-08-04 14:34:51 +0800

    文件上传

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,5 +1,21 @@
 <template>
 <div>
+      <el-upload
+        ref="upload"
+        style="display: inline"
+        drag
+        :headers="headers"
+        action="http://localhost:8000/api/project/upload/"
+        :on-preview="handlePreview"
+        :http-request="handleUploadForm"
+        :auto-upload="false"
+        :on-remove="handleRemove"
+        :before-remove="beforeRemove"
+        multiple
+        :file-list="fileList">
+      <el-link icon="el-icon-paperclip" type="primary">添加需要上传的文件</el-link>
+    </el-upload>
+  <el-button type="primary" @click="upload">上传</el-button>
   <el-upload
   class="upload-demo"
   action="http://localhost:8000/api/project/upload/"
@@ -7,8 +23,7 @@
   :on-preview="handlePreview"
   :on-remove="handleRemove"
   :before-remove="beforeRemove"
-  multiple
-  :file-list="fileList">
+  multiple>
   <el-button size="small" type="primary">点击上传</el-button>
 </el-upload>
 </div>
@@ -20,11 +35,9 @@
         headers:{
           Authorization:localStorage.token
         },
-        fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
       };
     },
-    created(){
-        
+    created(){      
     },
     methods: {
       handleRemove(file, fileList) {
@@ -35,7 +48,42 @@
       },
       beforeRemove(file, fileList) {
         return this.$confirm(`确定移除 ${ file.name }？`);
-      }
+      },
+      upload(){
+        this.$refs.upload.submit();
+      },
+      handleUploadForm(param) {
+      let thisInfo = this
+      let formData = new FormData()
+      // 在formData中加入我们需要的参数
+      formData.append('file', param.file)
+    	formData.append('projectid', this.$store.state.projectid)
+      formData.append('email',this.$store.state.mailbox)
+    // 向后端发送数据
+      thisInfo.$axios.post('/api/project/upload/', formData).
+        then(res => {              /* res 是 response 的缩写 */
+        switch (res.data.errno) {
+          case 0:
+            this.$message.success("上传成功!")
+            break;
+          case 11001:
+            this.$message.error("上传失败!");
+            break;
+        }
+      })
+      .catch(err => {
+        console.log(err);         /* 若出现异常则在终端输出相关信息 */
+      })
+      then((res) => {
+        if (res.status === 200) {
+          thisInfo.$message.success('修改信息成功')
+        } else {
+          thisInfo.$message.success('修改信息失败')
+        }
+        thisInfo.formFileList = []
+        thisInfo.uploadFormFileList = []
+      })
+    }
     }
   }
 </script>
